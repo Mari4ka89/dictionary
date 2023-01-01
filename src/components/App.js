2f41e4f476ca4b769507b70a0338b9d03c14180c
Mari4ka89 <mari4ka89@gmail.com>
2023-01-01 21:03:40 +0200

    Implemented footer with source links

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Dictionary from "./Dictionary";
 import "./App.css";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <Dictionary defaultKeyword="sunset" />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <footer className="Footer text-center">
+      <a
+        href="https://github.com/Mari4ka89/dictionary"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Open-source code
+      </a>{" "}
+      by Mariia Bezrukava which is{" "}
+      <a
+        href="https://cool-profiterole-8ee7e5.netlify.app/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        hosted on Netlify
+      </a>
+    </footer>
+  );
+}
